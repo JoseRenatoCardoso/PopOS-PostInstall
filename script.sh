c349869cae08c47c88208b659710450de41cb13b
José Renato Cardoso De Campos <joserenatocardosodecampos@gmail.com>
2025-12-11 00:52:50 -0300

    Update for my use

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ set -e
 
 ## Update repository and upgrade system ##
 update_system(){
-  sudo apt update && sudo apt dist-upgrade -y
+  sudo apt update && sudo apt upgrade -y
 }
 
 ## Check internet connection ##
@@ -34,16 +34,11 @@ sudo apt update -y
 APT_PACKAGES=(
   gparted
   timeshift
-  vlc
   code
   git
   gh
   ubuntu-restricted-extras
   v4l2loopback-utils
-  gcc-12
-  g++-12
-  clang-15
-  golang-go
 )
 
 # ---------------------------------------------------------------------- #
@@ -72,39 +67,55 @@ install_flatpak_packages(){
 flatpak install flathub org.chromium.Chromium -y
 flatpak install flathub com.usebottles.bottles -y
 flatpak install flathub md.obsidian.Obsidian -y
-flatpak install flathub io.gitlab.news_flash.NewsFlash -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub io.github.shiftey.Desktop -y
+sleep2
 flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub com.github.tchx84.Flatseal -y
 flatpak install flathub org.gnome.gitlab.somas.Apostrophe -y
 flatpak install flathub cc.arduino.IDE2 -y
 flatpak install flathub io.github.flattool.Warehouse -y
+sleep 2
 flatpak install flathub me.iepure.devtoolbox -y
 flatpak install flathub dev.geopjr.Collision -y
 flatpak install flathub re.sonny.Eloquent -y
 flatpak install flathub org.nickvision.money -y
 flatpak install flathub com.anydesk.Anydesk -y
+sleep 2
 flatpak install flathub org.apache.netbeans -y
 flatpak install flathub com.github.xournalpp.xournalpp -y
 flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub org.gnome.Boxes -y
+sleep 2
 flatpak install flathub org.qbittorrent.qBittorrent -y
 flatpak install flathub org.telegram.desktop -y
 flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub io.github.kolunmi.Bazaar -y
+flatpak install flathub app.drey.Biblioteca -y
+sleep 2
+flatpak install flathub io.github.brunofin.Cohesion -y
+flatpak install flathub com.ranfdev.DistroShelf -y
+flatpak install flathub org.gnome.TextEditor -y
+flatpak install flathub org.gnome.Extensions -y
+flatpak install flathub com.freerdp.FreeRDP -y
+sleep 2
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+flatpak install flathub com.heroicgameslauncher.hgl -y
+flatpak install flathub net.lutris.Lutris -y
+flatpak install flathub io.missioncenter.MissionCenter -y
+sleep 2
+flatpak install flathub org.gnome.clocks -y
+flatpak install flathub com.valvesoftware.Steam -y
+flatpak install flathub com.github.joseexposito.touche -y
+flatpak install flathub com.github.unrud.VideoDownloader -y
 }
 
 ## Installing Snap packages ##
 
 install_snap_packages(){
-sudo apt update
-sudo apt install snapd
-sudo apt update
 
-echo -e "[INFO] - Installing snap packages"
-
-sudo snap install gcc-13
+#Optional use
 
 }
 
@@ -144,7 +155,6 @@ update_system -y
 flatpak update -y
 sudo apt autoclean -y
 sudo apt autoremove -y
-nautilus -q
 }
 
 remove_apt_locks
@@ -157,7 +167,8 @@ remove_apt_locks
 update_repository
 install_apt_packages
 install_flatpak_packages
-install_snap_packages
+remove_apt_locks
+#install_snap_packages #Optional!
 install_rclone
 configure_zoxide
 add_fastfetch_repository
